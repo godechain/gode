39945244a7682ae375865c98c6b7133ab423080b
Fendy <godechain@hotmail.com>
2023-05-24 16:00:04 +0800

    pconsensus_v2

---
+++ consensus/pconsensus_v2.go
@@ -0,0 +1,40 @@
+
+// meeting 11 : luckyhash upgrade to support multi-chain(BSC , TRON)
+
+// meeting 12 : metagod wallet testing
+
+// meeting 13 : all node auto check
+
+// meeting 14 : load balancing of rpc and explorer
+
+// meeting 15 : metagod v1.0 testing
+
+// meeting 16 : metagod ios v1.0 testing
+
+// meeting 17 : mid-autumn gode talk
+
+// meeting 18 : metagod apk tesing round 2
+
+// meeting 19 : luckyhash multi-chain solutions
+
+// meeting 20 : lucyhash v2 conversation
+
+// meeting 21 : godechain POSA v2 extending
+
+// meeting 22 : metagod apk testing round 3
+
+// meeting 23 : chuangxing times
+
+// meeting 24 : godechian staking dapp testing round 1
+
+// meeting 25 : cross-chain bridge testing round 1
+
+// meeting 26 : cookieswap testing round 1
+
+// meeting 27 : metagod v2
+
+// meeting 28 : godepool v2 conversation
+
+// meeting 29 : metastar testing round 1
+
+// meeting 30 : 100% merge
